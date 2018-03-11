15de4d9a9e670b2a466edd31eee4c828ce37fb97
Paul Nicholsen <paulnicholsen27@gmail.com>
2018-03-11 09:55:48 -0400

    comments controller added and functional

--- javascript-assessment-master/src/controllers/comments.controller.js
+++ javascript-assessment-master/src/controllers/comments.controller.js
@@ -4,10 +4,27 @@ class CommentsController {
   }
 
   init() {
-    // kick off controller from here
+    $(".add-comment").each(function() {
+        $(this).on("submit", function(e){
+            e.preventDefault();
+            console.log('test');
+            var imageId = $(this).closest(".image").find("ul").attr("id")
+            var commentContents = $(this).find('input[name="comment-description"]').val();
+        })
+    })
   }
 
   addCommentFormListener() {
     // create comment form listener code here
   }
 }
+
+
+// $( ".selected" ).each(function(index) {
+//     $(this).on("click", function(){
+//         // For the boolean value
+//         var boolKey = $(this).data('selected');
+//         // For the mammal value
+//         var mammalKey = $(this).attr('id'); 
+//     });
+// });.find('input[name="FirstName"]').val();
