2adece2e177887047bfde4a6fb69cce2a4d16d96
Paul Nicholsen <paulnicholsen27@gmail.com>
2018-03-11 18:49:26 -0400

    works pending refactoring

--- javascript-assessment-master/src/controllers/comments.controller.js
+++ javascript-assessment-master/src/controllers/comments.controller.js
@@ -4,27 +4,32 @@ class CommentsController {
   }
 
   init() {
-    $(".add-comment").each(function() {
-        $(this).on("submit", function(e){
-            e.preventDefault();
-            console.log('test');
-            var imageId = $(this).closest(".image").find("ul").attr("id")
-            var commentContents = $(this).find('input[name="comment-description"]').val();
-        })
+    $(".add-comment").each((index, element) => {
+        element.addEventListener("submit", this.addCommentFormListener);
     })
-  }
-
-  addCommentFormListener() {
-    // create comment form listener code here
   }
 }
 
+CommentsController.prototype.addCommentFormListener = function(e){
+    e.preventDefault();
+    var image = $(this).closest(".image");
+    var imageId = Number(image.find("ul").attr("data-id"));
+    var commentForm = $(this).find('input[name="comment-description"]')
+    var commentContents = commentForm.val();
+    if (commentContents.length) {
+       var newComment = new Comment(imageId, commentContents);
+       CommentsController.render(newComment);
+       commentForm.val("");
+    }
+}
 
-// $( ".selected" ).each(function(index) {
-//     $(this).on("click", function(){
-//         // For the boolean value
-//         var boolKey = $(this).data('selected');
-//         // For the mammal value
-//         var mammalKey = $(this).attr('id'); 
-//     });
-// });.find('input[name="FirstName"]').val();
+
+
+CommentsController.render = function(newComment){
+    var imageId = newComment.imageId;
+    var image = Image.all.find(image => image.id === Number(imageId));
+    image.comments.push(newComment);
+    var commentList = $(`#comments-${imageId}`);
+    var commentEl = newComment.buildCommentEl();
+    commentList.append(commentEl);
+}
--- javascript-assessment-master/src/models/comment.js
+++ javascript-assessment-master/src/models/comment.js
@@ -1,1 +1,14 @@
 // create Comment class here
+
+function Comment(imageId, text) {
+    this.id = this.constructor.all.length;
+    this.imageId = imageId;
+    this.text = text;
+    this.constructor.all.push(this);
+}
+
+Comment.all = [];
+
+Comment.prototype.buildCommentEl = function() {
+    return $(`<li id='comment-${this.id}'>${this.text}</li>`);
+}
