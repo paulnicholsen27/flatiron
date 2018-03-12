a85f63ba54bd7fde7b04117df25cb04f470872ba
Paul Nicholsen <paulnicholsen27@gmail.com>
2018-03-12 11:53:12 -0400

    some refactoring and comments

--- javascript-assessment-master/src/controllers/comments.controller.js
+++ javascript-assessment-master/src/controllers/comments.controller.js
@@ -4,32 +4,32 @@ class CommentsController {
   }
 
   init() {
-    $(".add-comment").each((index, element) => {
+      // add comment form listener to every comment form element
+      this.$addCommentForm.each((index, element) => {
         element.addEventListener("submit", this.addCommentFormListener);
     })
   }
 }
 
 CommentsController.prototype.addCommentFormListener = function(e){
-    e.preventDefault();
+    e.preventDefault();  // don't submit form and reload page
     var image = $(this).closest(".image");
     var imageId = Number(image.find("ul").attr("data-id"));
     var commentForm = $(this).find('input[name="comment-description"]')
     var commentContents = commentForm.val();
-    if (commentContents.length) {
+    if (commentContents.length) {  // don't create comment if nothing was entered
        var newComment = new Comment(imageId, commentContents);
        CommentsController.render(newComment);
-       commentForm.val("");
+       commentForm.val("");  // reset comment form
     }
 }
 
-
-
 CommentsController.render = function(newComment){
+    // adds comment to Image object and displays it in comment list
     var imageId = newComment.imageId;
-    var image = Image.all.find(image => image.id === Number(imageId));
+    var image = Image.all.find(image => image.id === imageId);
     image.comments.push(newComment);
     var commentList = $(`#comments-${imageId}`);
     var commentEl = newComment.buildCommentEl();
-    commentList.append(commentEl);
+    commentList.prepend(commentEl);  // newest comments on top
 }
--- javascript-assessment-master/src/models/comment.js
+++ javascript-assessment-master/src/models/comment.js
@@ -1,10 +1,9 @@
-// create Comment class here
-
 function Comment(imageId, text) {
+    // create the next incremented id based on length of existing comments
     this.id = this.constructor.all.length;
     this.imageId = imageId;
     this.text = text;
-    this.constructor.all.push(this);
+    this.constructor.all.push(this);  // add new Comment to list of comments
 }
 
 Comment.all = [];
